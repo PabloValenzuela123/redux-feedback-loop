7d6f5ef1b22bec6b725dc552fef8aaa53b309bf9
Pablo Valenzuela <p.valenzuela17@gmail.com>
2020-06-15 10:21:02 -0500

    pushing index again

--- src/index.js
+++ src/index.js
@@ -3,6 +3,54 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//taking from redux 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialFeedback = {
+    feeling: '',
+    understanding: '',
+    support: '',
+    comments: ''
+}
+
+//Get with reducers
+const feedbackReducer = (state = [], action) => {
+    if (action.type === 'SET_FEEDBACK') {
+        return action.payload
+    }
+    return state;
+}
+
+// Array of every answers 
+const finalSubmitReducer = (state = initialFeedback, action) => {
+    if (action.type === 'ADD_FEELINGS') {
+        return { ...state, feeling: action.payload };
+    }
+    if (action.type === 'ADD_UNDERSTANDING') {
+        return { ...state, understanding: action.payload };
+    }
+    if (action.type === 'ADD_SUPPORT') {
+        return { ...state, support: action.payload };
+    }
+    if (action.type === 'ADD_COMMENTS') {
+        return { ...state, comments: action.payload };
+    }
+
+    return state;
+};
+
+// Store
+const storeInstance = createStore(
+    combineReducers({
+        feedbackReducer,
+        finalSubmitReducer
+    }),
+    applyMiddleware(logger)
+);
+
+// Connect mapStateToProps & reduxState for sending data to components
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
